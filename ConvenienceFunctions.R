fa70e596e302525f3f9fea63a73806affa035c72
Carl de Boer <Carldeboer@users.noreply.github.com>
2016-06-12 09:49:47 -0400

    Create ConvenienceFunctions.R

---
+++ ConvenienceFunctions.R
@@ -0,0 +1,6 @@
+randomRows = function(x, n=10){ x[sample(n,nrow(x)),]}
+
+na.rm = function(x){ x[!is.na(x)]}
+naIsFalse = function(x){x[is.na(x)]=F; return x}
+
+corner = function(x){t(head(t(head(x))))}
