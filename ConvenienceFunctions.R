f7dbd8d509be7b44b6107cc990d5ca08169e153b
Carl de Boer <Carldeboer@users.noreply.github.com>
2016-06-13 09:29:18 -0400

    Update ConvenienceFunctions.R

--- ConvenienceFunctions.R
+++ ConvenienceFunctions.R
@@ -3,4 +3,4 @@ randomRows = function(x, n=10){ x[sample(nrow(x),n),]}
 na.rm = function(x){ x[!is.na(x)]}
 naIsFalse = function(x){x[is.na(x)]=F; return(x)}
 
-corner = function(x, ...){t(head(t(head(x, ...)), ...))}
+corner = function(x, n=10){x[1:(min(c(nrow(x),n))),1:(min(c(ncol(x),n)))]}
