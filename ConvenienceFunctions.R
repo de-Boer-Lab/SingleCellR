112af79e95630802b26db30148393e2593a1cfa1
Carl de Boer <Carldeboer@users.noreply.github.com>
2016-06-15 15:39:33 -0400

    Update ConvenienceFunctions.R

--- ConvenienceFunctions.R
+++ ConvenienceFunctions.R
@@ -1,6 +1,9 @@
 randomRows = function(x, n=10){ x[sample(nrow(x),n),]}
 
 na.rm = function(x){ x[!is.na(x)]}
+
 naIsFalse = function(x){x[is.na(x)]=F; return(x)}
 
 corner = function(x, n=10){x[1:(min(c(nrow(x),n))),1:(min(c(ncol(x),n)))]}
+
+combinePFisher = function(x){pchisq( -2*sum(log(x)), 2*length(x), lower.tail=FALSE)}
