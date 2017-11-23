de4a8d0d1e104777a29948ecee7a17d36fa9fa56
Carl de Boer <Carldeboer@users.noreply.github.com>
2017-11-23 08:55:27 -0500

    Create ggFlowCytometry.R

---
+++ ggFlowCytometry.R
@@ -0,0 +1,33 @@
+#makes a polygonal gate on a ggplot
+#note that this needs some calibration that I haven't quite figured out, so the points you click on are not quite the correct locations on the graph
+fcsGate = function(p,n=4, draw=T){
+  require(grid)
+  pData = ggplot_build(p)
+  xrange=pData$layout$panel_ranges[[1]]$x.range
+  yrange=pData$layout$panel_ranges[[1]]$y.range
+  xName=as.character(pData$plot$mapping$x)
+  yName=as.character(pData$plot$mapping$y)
+  points = data.frame(n=1:n)
+  points[[xName]]=NA;
+  points[[yName]]=NA;
+  seekViewport(grid.ls()[[1]])
+  for(i in 1:n){
+    y <-  grid.locator("npc")
+    y <- as.numeric(substring(y, 1, nchar(y)-3))
+    message(sprintf("x=%g, y=%g",y[1],y[2]))
+    #points[[xName]][i] <- min(xrange) + y[1]*diff(xrange)
+    #points[[yName]][i] <- min(yrange) + y[2]*diff(yrange)
+    points[[xName]][i] <- min(xrange) + y[1]/0.75*diff(xrange)
+    points[[yName]][i] <- min(yrange) + y[2]/0.75*diff(yrange)
+    if(i>1 && draw){
+      p = p+geom_segment(x=points[[xName]][i-1], y=points[[yName]][i-1], xend=points[[xName]][i], yend=points[[yName]][i], colour="red"); print(p)
+    }
+  }
+  return(points);
+}
+
+#returns a logical vector containing whether each point in the dataframe is within the given polygon (made with fcsGate)
+isWithin = function(data, polygon){
+  require(sp)
+  point.in.polygon(data[[names(polygon)[2]]], data[[names(polygon)[3]]], polygon[[2]], polygon[[3]])
+}
